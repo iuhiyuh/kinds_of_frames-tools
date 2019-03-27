fe1ec73f686eeec57d2f670277c19326949222e4
iuhiyuh <58005994@qq.com>
2019-03-27 17:34:56 +0800

    fix

--- main_single.py
+++ main_single.py
@@ -10,24 +10,24 @@ opt.target_path = opt.target_path[0]
 opt.cut_start = opt.cut_start[0]
 opt.cut_end = opt.cut_end[0]
 
-i = 0
 cut_start = opt.cut_start
 cut_end = opt.cut_end
 
 if not os.path.exists("".join(opt.target_path)):
     os.mkdir("".join(opt.target_path))
 cap = cv2.VideoCapture(opt.videos_path)
-success_get, frame = cap.read()
+i = 0
 while True:
     success_get, frame = cap.read()
     if success_get:
         i += 1
-        if cut_start - 1 <= i & i <= cut_end - 1:
-            print("load: 第 %d 帧 / 从 %d 帧，到 %d 帧" % (i + 1, cut_start, cut_end))
-            cv2.imencode(os.path.join(opt.target_path, str(i) + '.png'), frame)[1] \
-                .tofile(os.path.join(opt.target_path, str(i) + '.png'))
+        if cut_start < i & i < cut_end+2:
+            print("load: 第 %d 帧 / 从 %d 帧，到 %d 帧" % (i-1, cut_start, cut_end))
+            cv2.imencode(os.path.join(opt.target_path, str(i-1) + '.png'), frame)[1]\
+                .tofile(os.path.join(opt.target_path, str(i-1) + '.png'))
+        if i >= cut_end+2:
+            break
     else:
         break
-
     cv2.waitKey(1)
 cap.release()
