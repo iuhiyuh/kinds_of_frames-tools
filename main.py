422b49e93b51018056856b4af95568cc4bdf047d
iuhiyuh <58005994@qq.com>
2019-03-29 16:35:50 +0800

    optimization for long video
    增加对长视频处理的优化

--- main.py
+++ main.py
@@ -19,25 +19,28 @@ if not os.path.exists(opt.target_path):
 for video in os.listdir(opt.videos_path):
     if ('.avi' or '.mp4') not in video:
       continue
+    
     name, ext = os.path.splitext(video)
     opt.target_path = os.path.join(opt.target_path, name)
     if not os.path.exists(opt.target_path):
         os.mkdir(opt.target_path)
+    
     cap = cv2.VideoCapture(os.path.join(opt.videos_path,video))
-    i = 0
+    cap.set(cv2.CAP_PROP_POS_FRAMES, cut_start)
+
+    i = cut_start
     while True:
         success_get, frame = cap.read()
         if success_get:
             i += 1
-            if cut_start < i & i < cut_end+2:
-                print("load: 第 %d 帧 / 从 %d 帧，到 %d 帧" % (i-1, cut_start, cut_end))
+            if cut_start < i & i < cut_end + 2:
+                print("load: 第 %d 帧 / 从 %d 帧，到 %d 帧" % (i - 1, cut_start, cut_end))
                 cv2.imencode(os.path.join(opt.target_path, str(i-1) + '.png'), frame)[1]\
-                    .tofile(os.path.join(opt.target_path, str(i-1) + '.png'))
-            if i >= cut_end+2:
+                .tofile(os.path.join(opt.target_path, str(i-1) + '.png'))
+            if i >= cut_end + 2:
                 break
         else:
             break
-
         cv2.waitKey(1)
     cap.release()
 
--- main_single.py
+++ main_single.py
@@ -16,16 +16,18 @@ cut_end = opt.cut_end
 if not os.path.exists("".join(opt.target_path)):
     os.mkdir("".join(opt.target_path))
 cap = cv2.VideoCapture(opt.videos_path)
-i = 0
+cap.set(cv2.CAP_PROP_POS_FRAMES, cut_start)
+
+i = cut_start
 while True:
     success_get, frame = cap.read()
     if success_get:
         i += 1
-        if cut_start < i & i < cut_end+2:
-            print("load: 第 %d 帧 / 从 %d 帧，到 %d 帧" % (i-1, cut_start, cut_end))
+        if cut_start < i & i < cut_end + 2:
+            print("load: 第 %d 帧 / 从 %d 帧，到 %d 帧" % (i - 1, cut_start, cut_end))
             cv2.imencode(os.path.join(opt.target_path, str(i-1) + '.png'), frame)[1]\
                 .tofile(os.path.join(opt.target_path, str(i-1) + '.png'))
-        if i >= cut_end+2:
+        if i >= cut_end + 2:
             break
     else:
         break
